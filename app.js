84460b48ba7882ec9b370977f61f733e8fd259f1
wangaw@94gmail.com <wangaw94@gmail.com>
2022-07-31 21:58:20 +0300

    Length of the shortest word in a string

---
+++ app.js
@@ -0,0 +1,30 @@
+//GIVEN A STRING OF WORDS RETURN THE LENGTH OF THE SHORTEST WORD(s)
+
+// let S = 'My name isk Wilson Omondi Wanga, My aspiration is to become an software ENGINEER'
+
+// function short(k){
+//  const arrS = k.split(' ');
+
+//  arrS.sort((a, b) => a.length - b.length);
+
+//  return arrS[0];
+// }
+
+// console.log(short(s));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
